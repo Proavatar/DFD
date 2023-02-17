858d50e11f0bb0c0b4dccbb9e2424983c26b87b2
Fred Dijkstra <fred@computerguided.com>
2023-02-17 14:24:59 +0100

    Process screen

---
+++ DFD/Screens/ProcessViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ProcessViewController.swift
+//  DFD
+//
+//  Created by Fred Dijkstra on 17/02/2023.
+//
+
+import UIKit
+import DataflowDiagramSDK
+
+// ------------------------------------------------------------------------------------------------
+class ProcessViewController: UIViewController
+{
+    // --------------------------------------------------------------------------------------------
+    @IBOutlet weak var diagramFileLabel : UILabel!
+    @IBOutlet weak var dataFileLabel : UILabel!
+    @IBOutlet weak var diagramOutputTextView : UITextView!
+    
+    // --------------------------------------------------------------------------------------------
+    weak var  dataflowControl : DataflowControl!
+
+    // --------------------------------------------------------------------------------------------
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        diagramFileLabel.text = dataflowControl.diagramFile
+        dataFileLabel.text    = dataflowControl.dataFile
+    }
+    
+    // --------------------------------------------------------------------------------------------
+    @IBAction func processButtonPressed(_ sender: Any)
+    {
+        let diagramInputs = dataflowControl.diagramInputs
+        
+        dataflowControl.diagram.processAllUpdates( diagramInputs: diagramInputs )
+    }
+    
+    // --------------------------------------------------------------------------------------------
+    func allUpdatesProcessed()
+    {
+        for diagramOutput in dataflowControl.diagramOutputs
+        {
+            for name in diagramOutput.outputs.keys
+            {
+                let value = diagramOutput.outputs[name]!
+                let valueString = getValueString( value )
+                diagramOutputTextView.text.append( "\(diagramOutput.timestamp),\(name),\(valueString)\n" )
+            }
+        }
+    }
+
+}
